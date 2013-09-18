38a9aad9b6d7a3fb52a752f67ac17b39be7b4da3
hsvallfors <hsvallfors>
2013-09-18 13:00:48 +0000

    Started stub for DeltaVStoreDisk.

---
+++ src/main/java/se/repos/deltav/store/DeltaVStoreDisk.java
@@ -0,0 +1,34 @@
+package se.repos.deltav.store;
+
+import org.w3c.dom.Document;
+
+import se.simonsoft.cms.item.CmsItemId;
+import se.simonsoft.cms.item.RepoRevision;
+
+public class DeltaVStoreDisk implements DeltaVStore {
+
+	@Override
+	public void put(CmsItemId resource, Document deltav) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean has(CmsItemId resouce) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public RepoRevision getHighestCalculated(CmsItemId resource) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Document get(CmsItemId resource) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
